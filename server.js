91e8c6bcccc1b7d44afcac9ad9bed7105b28d978
Glitch (smsapi) <none>
2022-07-27 12:36:02 +0000

    🕹️👕 Updated with Glitch

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 
 // Create Express Server
 const app = express();
+
+// enable request from any origin
 app.use(cors());
+
+// for parsing application/json
 app.use(express.json());
 
 // Logging
@@ -33,60 +37,55 @@ app.use(
   createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
-    on: {
-      onProxyReq: function onProxyReq(proxyReq, req, res) {
-        proxyReq.setHeader(
-          "x-forwarded-for",
-          req.headers["x-forwarded-for"].split(",")[0]
-        );
-        proxyReq.setHeader(
-          "origin",
-          req.headers["origin"] ?? "//" + process.env.APP_ORIGIN
-        );
-        if (req.method === "POST") {
-          // Make any needed POST parameter changes
-          let body = new Object();
+    onProxyReq: function onProxyReq(proxyReq, req, res) {
+      proxyReq.setHeader(
+        "x-forwarded-for",
+        req.headers["x-forwarded-for"].split(",")[0]
+      );
+      proxyReq.setHeader(
+        "origin",
+        req.headers["origin"] ?? "//" + process.env.APP_ORIGIN
+      );
+      if (req.method === "POST") {
+        // Make any needed POST parameter changes
+        let body = new Object();
 
-          if (req.path === "/user/login/") {
-            let postBody = req.body;
-            const ip = req.ip.replace("::ffff:", "") ?? "127.0.0.1";
-            body.client_ip = ip;
-            body["api_key"] = process.env.APP_KEY;
-            body.origin = process.env.APP_ORIGIN;
+        if (req.path === "/user/login/") {
+          let postBody = req.body;
+          const ip = req.ip.replace("::ffff:", "") ?? "127.0.0.1";
+          body.client_ip = ip;
+          body["api_key"] = process.env.APP_KEY;
+          body.origin = process.env.APP_ORIGIN;
 
-            body = { ...body, ...postBody };
-          }
-
-          // URI encode JSON object
-          body = Object.keys(body)
-            .map(function (key) {
-              return (
-                encodeURIComponent(key) + "=" + encodeURIComponent(body[key])
-              );
-            })
-            .join("&");
-
-          // Update header
-          proxyReq.setHeader(
-            "content-type",
-            "application/x-www-form-urlencoded"
-          );
-          proxyReq.setHeader("content-length", body.length);
-
-          // Write out body changes to the proxyReq stream
-          proxyReq.write(body);
-          proxyReq.end();
+          body = { ...body, ...postBody };
         }
-      },
-      proxyRes: (proxyRes, req, res) => {
-        /* handle proxyRes */
-      },
-      error: (err, req, res) => {
-        res.json({
-          error: 500,
-          msg: "Something went wrong on the proxy server.",
-        });
-      },
+
+        // URI encode JSON object
+        body = Object.keys(body)
+          .map(function (key) {
+            return (
+              encodeURIComponent(key) + "=" + encodeURIComponent(body[key])
+            );
+          })
+          .join("&");
+
+        // Update header
+        proxyReq.setHeader("content-type", "application/x-www-form-urlencoded");
+        proxyReq.setHeader("content-length", body.length);
+
+        // Write out body changes to the proxyReq stream
+        proxyReq.write(body);
+        proxyReq.end();
+      }
+    },
+    proxyRes: (proxyRes, req, res) => {
+      /* handle proxyRes */
+    },
+    error: (err, req, res) => {
+      res.json({
+        error: 500,
+        msg: "Something went wrong on the proxy server.",
+      });
     },
   })
 );
